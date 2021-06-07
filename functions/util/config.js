f4b9c53389e52d6288dfdb2ef296c75bbd4f30e5
Alex Lapin <lapinalexandr@yandex.ru>
2021-06-08 01:00:06 +0300

    Full stack backend for firebase

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyATUn2fazbDtm092mrRV4l41p8VzNvE4iI",
+    authDomain: "socialapp-1d224.firebaseapp.com",
+    projectId: "socialapp-1d224",
+    storageBucket: "socialapp-1d224.appspot.com",
+    messagingSenderId: "1052199724884",
+    appId: "1:1052199724884:web:401ae8cbcbbda5d03bcac1",
+    measurementId: "G-X9254ZWQPV"
+  };
